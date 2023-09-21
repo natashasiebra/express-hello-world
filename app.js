ecb8f49b8016fc9388731397195212fc846e6095
natashasiebra <107592843+natashasiebra@users.noreply.github.com>
2023-09-21 13:51:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.get('/req', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+    console.log("Just got a request!");
+    res.send('Yo!');
+});
+
+app.get('/meunome', (req, res) => {
+  const meuNome = "Fábio Duarte de Oliveira";
+  res.send(`Meu nome é ${meuNome}`);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 
 const html = `
 <!DOCTYPE html>
