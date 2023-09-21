eb475621b3a9b05e79343c854ff5159147749a9a
natashasiebra <107592843+natashasiebra@users.noreply.github.com>
2023-09-21 14:08:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.get('/req', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
     console.log("Just got a request!");
     res.send('Yo!');
 });
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.get('/meunome', (req, res) => {
-  const meuNome = "Natasha Siebra";
+  const meuNome = "Fábio Duarte de Oliveira";
   res.send(`Meu nome é ${meuNome}`);
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const html = `
